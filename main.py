b8324a50279fa08df14c1002c88d005c27f27f06
FreakJazz <jazzrb2307@hotmail.com>
2022-06-18 19:06:54 -0500

    add async for cross_validation

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ Description:
 This is a backend for EEG data with IA two methods 
 use framework FASTAPI and AIRTABLE DATABASE
 """
-
+import asyncio
 from datetime import datetime
+import time
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -45,105 +46,80 @@ app = FastAPI(title= 'IA EEG',
 class Data(BaseModel):
     name: str
 
-
     class Config:
         orm_mode = True
 
-    def get_val_electrodes(cross):
-        n_blocks = 20;
-        limit = 0.05 
-        count = 2
-        runs = 6
-        folder_data = 'sessions'
-        entries = os.listdir(folder_data)
-        se = ['s1.mat', 's2.mat', 's3.mat', 's4.mat']
-        n_correct_final = np.zeros((4, 20))
-        accuracy = []
-        
-        for s in range(4):
-            session_test = se[s]
-            session_training = se[:s] + se[s+1:]
-            # print('n', session_training)
-            # print('session_test',session_test)
-            
-            X_train = np.empty((32, 32, 0))
-            y_train = np.empty((0))
-            
-            X_test = np.empty((32, 32, 0))
-            y_test = np.empty((0))
-            
-            scores = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-            n_correct = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        
-            # Data Training
-            for entry in session_training:
-                file_selected = folder_data + '/' + entry
-                count = count + 1
+@app.middleware("http")
+async def add_process_time_header(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(f'{process_time:0.4f} sec')
+    return response
 
-                if file_selected is not 'session_training/.DS_Store':
-                    # print(file_selected)
-                    data_session = loadmat(file_selected, squeeze_me=True, struct_as_record=False)
-                    for i in range(6):
-                        session = data_session['runs'][i]
-                        X_train = np.concatenate((X_train, session.x), axis=2)
-                        y_train = np.concatenate((y_train, session.y), axis=0)
-                    
-            
-            winsorized_x = winsorize(X_train[k,:,:], limits=limit) 
-            normalize_x = stats.zscore(winsorized_x, axis=None)
-            
-            X_train = normalize_x.T
-            
-            if cross.solver == 'svd':
-                clf = LinearDiscriminantAnalysis(solver=cross.solver, tol=cross.tol, store_covariance=False)
-            else:
-                clf = LinearDiscriminantAnalysis(solver=cross.solver, tol=cross.tol, store_covariance=False, shrinkage=cross.shrinkage)
-
-            clf.fit(X_train, y_train)
-            
-            
-            
-            file_selected = folder_data + '/' + session_test
-            
-            
-            data_session = loadmat(file_selected, squeeze_me=True, struct_as_record=False)
-            for i in range(6):
-                session = data_session['runs'][i]
-                X_test = session.x
-                
-                winsorized_x_test = winsorize(X_test[k,:,:], limits=limit)
-                normalize_x_test = stats.zscore(winsorized_x_test, axis=None)
-                X_test = normalize_x_test.T
-                y_test = clf.decision_function(X_test)
-                
-                
-                for j in range(20):
-                    start = (j)*6;
-                    stop  = (j+1)*6;
-                    stimulus_sequence = session.stimuli[start:stop]
-                    new_stimulus_sequence = stimulus_sequence - 1
-                    # print('new_stimulus_sequence', new_stimulus_sequence)
-                    aa = scores[new_stimulus_sequence]
-                    aa = aa[:,None]
-                    bb = y_test[start:stop]
-                    cc = np.add(aa, bb)
-                    np.put(scores, new_stimulus_sequence, cc)
-                    # print('scores', scores)
-                    ind = np.argmax(scores) + 1
-                    # print('ind', ind)
-                    # print(session.target, ind)
-                    if ind == session.target:
-                        n_correct[j] = n_correct[j] + 1
-                        # print('n_correct', n_correct)
-            # print('n_correct', n_correct)
-
-            
-            n_correct_final[s,:] = n_correct
-            mean_column_session = n_correct_final.mean(axis=0)
-            
-            acc = mean_column_session/3
+async def get_val_electrodes(cross: dict, k: int, accuracy: dict):
+    limit = 0.05 
+    count = 2
+    folder_data = 'sessions'
+    se = ['s1.mat', 's2.mat', 's3.mat', 's4.mat']
+    n_correct_final = np.zeros((4, 20))
+    for s in range(4):
+        session_test = se[s]
+        session_training = se[:s] + se[s+1:]
+        X_train = np.empty((32, 32, 0))
+        y_train = np.empty((0))
+        X_test = np.empty((32, 32, 0))
+        y_test = np.empty((0))
+        scores = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        n_correct = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        # Data Training
+        for entry in session_training:
+            file_selected = folder_data + '/' + entry
+            count = count + 1
+            if file_selected is not 'session_training/.DS_Store':
+                # print(file_selected)
+                data_session = loadmat(file_selected, squeeze_me=True, struct_as_record=False)
+                for i in range(6):
+                    session = data_session['runs'][i]
+                    X_train = np.concatenate((X_train, session.x), axis=2)
+                    y_train = np.concatenate((y_train, session.y), axis=0)
+        winsorized_x = winsorize(X_train[k,:,:], limits=limit) 
+        normalize_x = stats.zscore(winsorized_x, axis=None)
+        X_train = normalize_x.T
+        print ("cross", k)
+        solver = cross['solver']
+        if solver == 'svd':
+            clf = LinearDiscriminantAnalysis(solver=cross['solver'], tol=cross['tol'], store_covariance=False)
+        else:
+            clf = LinearDiscriminantAnalysis(solver=cross['solver'], tol=cross['tol'], store_covariance=False, shrinkage=cross['shrinkage'])
+        clf.fit(X_train, y_train)
+        file_selected = folder_data + '/' + session_test
+        data_session = loadmat(file_selected, squeeze_me=True, struct_as_record=False)
+        for i in range(6):
+            session = data_session['runs'][i]
+            X_test = session.x
+            winsorized_x_test = winsorize(X_test[k,:,:], limits=limit)
+            normalize_x_test = stats.zscore(winsorized_x_test, axis=None)
+            X_test = normalize_x_test.T
+            y_test = clf.decision_function(X_test)
+            for j in range(20):
+                start = (j)*6;
+                stop  = (j+1)*6;
+                stimulus_sequence = session.stimuli[start:stop]
+                new_stimulus_sequence = stimulus_sequence - 1
+                aa = scores[new_stimulus_sequence]
+                aa = aa[:,None]
+                bb = y_test[start:stop]
+                cc = np.add(aa, bb)
+                np.put(scores, new_stimulus_sequence, cc)
+                ind = np.argmax(scores) + 1
+                if ind == session.target:
+                    n_correct[j] = n_correct[j] + 1
+        n_correct_final[s,:] = n_correct
+        mean_column_session = n_correct_final.mean(axis=0)
+        acc = mean_column_session/3
+    accuracy[str(k)] = np.mean(acc)*100
     
-
 @app.post('/api/electrodes/cross_validation/')
 async def analisis_cross_validation(cross: CrossValidation):
 
@@ -157,9 +133,39 @@ async def analisis_cross_validation(cross: CrossValidation):
         "tol":tol, 
         "shrinkage":shrinkage
     }
-
-            
-    accuracy.append(np.mean(acc)*100)
+    accuracy = dict = {}
+    await asyncio.gather(get_val_electrodes(cross, 0, accuracy)
+                        , get_val_electrodes(cross, 1, accuracy)
+                        , get_val_electrodes(cross, 2, accuracy)
+                        , get_val_electrodes(cross, 3, accuracy)
+                        , get_val_electrodes(cross, 4, accuracy)
+                        , get_val_electrodes(cross, 5, accuracy)
+                        , get_val_electrodes(cross, 6, accuracy)
+                        , get_val_electrodes(cross, 7, accuracy)
+                        , get_val_electrodes(cross, 8, accuracy)
+                        , get_val_electrodes(cross, 9, accuracy)
+                        , get_val_electrodes(cross, 10, accuracy)
+                        , get_val_electrodes(cross, 11, accuracy)
+                        , get_val_electrodes(cross, 12, accuracy)
+                        , get_val_electrodes(cross, 13, accuracy)
+                        , get_val_electrodes(cross, 14, accuracy)
+                        , get_val_electrodes(cross, 15, accuracy)
+                        , get_val_electrodes(cross, 16, accuracy)
+                        , get_val_electrodes(cross, 17, accuracy)
+                        , get_val_electrodes(cross, 18, accuracy)
+                        , get_val_electrodes(cross, 19, accuracy)
+                        , get_val_electrodes(cross, 20, accuracy)
+                        , get_val_electrodes(cross, 21, accuracy)
+                        , get_val_electrodes(cross, 22, accuracy)
+                        , get_val_electrodes(cross, 23, accuracy)
+                        , get_val_electrodes(cross, 24, accuracy)
+                        , get_val_electrodes(cross, 25, accuracy)
+                        , get_val_electrodes(cross, 26, accuracy)
+                        , get_val_electrodes(cross, 27, accuracy)
+                        , get_val_electrodes(cross, 28, accuracy)
+                        , get_val_electrodes(cross, 29, accuracy)
+                        , get_val_electrodes(cross, 30, accuracy)
+                        , get_val_electrodes(cross, 31, accuracy))
 
     print('accuracy',accuracy)
 
@@ -172,49 +178,49 @@ async def analisis_cross_validation(cross: CrossValidation):
     {
     "fields": {
         "Date": date_time, 
-        "E1":  accuracy[0],
-        "E2":  accuracy[1],
-        "E3":  accuracy[2],
-        "E4":  accuracy[3],
-        "E5":  accuracy[4],
-        "E6":  accuracy[5],
-        "E7":  accuracy[6],
-        "E8":  accuracy[7],
-        "E9":  accuracy[8],
-        "E10": accuracy[9],
-        "E11": accuracy[10],
-        "E12": accuracy[11],
-        "E13": accuracy[12],
-        "E14": accuracy[13],
-        "E15": accuracy[14],
-        "E16": accuracy[15],
-        "E17": accuracy[16],
-        "E18": accuracy[17],
-        "E19": accuracy[18],
-        "E20": accuracy[19],
-        "E21": accuracy[20],
-        "E22": accuracy[21],
-        "E23": accuracy[22],
-        "E24": accuracy[23],
-        "E25": accuracy[24],
-        "E26": accuracy[25],
-        "E27": accuracy[26],
-        "E28": accuracy[27],
-        "E29": accuracy[28],
-        "E30": accuracy[29],
-        "E31": accuracy[30],
-        "E32": accuracy[31],
+        "E1":  accuracy["0"],
+        "E2":  accuracy["1"],
+        "E3":  accuracy["2"],
+        "E4":  accuracy["3"],
+        "E5":  accuracy["4"],
+        "E6":  accuracy["5"],
+        "E7":  accuracy["6"],
+        "E8":  accuracy["7"],
+        "E9":  accuracy["8"],
+        "E10": accuracy["9"],
+        "E11": accuracy["10"],
+        "E12": accuracy["11"],
+        "E13": accuracy["12"],
+        "E14": accuracy["13"],
+        "E15": accuracy["14"],
+        "E16": accuracy["15"],
+        "E17": accuracy["16"],
+        "E18": accuracy["17"],
+        "E19": accuracy["18"],
+        "E20": accuracy["19"],
+        "E21": accuracy["20"],
+        "E22": accuracy["21"],
+        "E23": accuracy["22"],
+        "E24": accuracy["23"],
+        "E25": accuracy["24"],
+        "E26": accuracy["25"],
+        "E27": accuracy["26"],
+        "E28": accuracy["27"],
+        "E29": accuracy["28"],
+        "E30": accuracy["29"],
+        "E31": accuracy["30"],
+        "E32": accuracy["31"],
         "METODO": "CROSS-VALIDATION"
     }
     }
     ]
     }
-    
-
+  
     r = requests.post(endpoint, json=data, headers=headers)
     data = accuracy
     return data
 
+#########################PORCENTAJE##################################
 @app.post('/api/electrodes/porcentage/')
 async def analisis_cross_validation(porcentage: Porcentage):
     now = datetime.now() # current date and time
